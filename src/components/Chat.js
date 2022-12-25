26ae9c55c6e7a62ff92c65dd06c37094409d1667
unknown <mm.farid@post.cz>
2022-12-25 17:58:04 +0100

    final edit at redesign

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,29 +1,5 @@
 import {useEffect, useState, useRef} from 'react'
 import { database} from "../firebase.js"
-<<<<<<< HEAD
-import { ref, onValue, set, update, push } from "firebase/database";
-import {useList} from 'react-firebase-hooks/database';
-import Message from './Message.js';
-
-
-const Chat = ({room, playerId}) => {
-  const [name, setName] = useState();
-  const [isMine, setIsMine] = useState(false);
-  const messageInputRef = useRef();
-
-  let roomChatRef = ref(database, `chats/${room}`);
-  let newMessageKey = push(roomChatRef).key;
-
-  const [messages, loading, error] = useList(roomChatRef);
-  const messagesEndRef = useRef(null);
-  
-  // 1. Function that runs when user sends the form
-  const sendMessage = () => {
-    if(messageInputRef.current.value !== '') {
-      const messageData = {
-        author: playerId,
-        body: messageInputRef.current.value,
-=======
 import { ref,  set, push } from "firebase/database";
 
 import {useList} from 'react-firebase-hooks/database';
@@ -45,17 +21,12 @@ const Chat = ({room, name, playerId}) => {
       const messageData = {
         author: playerId,
         body: inputRef.current.value,
->>>>>>> master
         name: name,
       }
 
       set(ref(database, 'chats/' + room + '/' + newMessageKey), messageData);
 
-<<<<<<< HEAD
-      messageInputRef.current.value = '';
-=======
       inputRef.current.value = '';
->>>>>>> master
 
     }
 
@@ -67,21 +38,7 @@ const Chat = ({room, name, playerId}) => {
       sendMessage();
    }
 
-<<<<<<< HEAD
-  // 3. Listen to the name for the form
-  useEffect(()=> {
-    onValue(ref(database, `players/${playerId}/name`), snapshot => {
-      if(snapshot) {
-        setName(snapshot.val());
-      } 
-    })
-  }, [])
-
-  // 4. A function to scroll to the last div in chat
-  const scrollToBottom = () => {
-=======
    const scrollToBottom = () => {
->>>>>>> master
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
   }
 
@@ -91,23 +48,6 @@ const Chat = ({room, name, playerId}) => {
     scrollToBottom();
   }, [messages]);
 
-<<<<<<< HEAD
-
-  return (
-    <div className='chat'>
-        <h3>Chat</h3>
-        <ul className='messages'>
-          {error && <strong>Error: {error}</strong>}
-          {loading && <span>List: Loading...</span>}
-          {(!loading && messages) && messages.map(msg => <Message key={msg.key} message={msg} playerId={playerId} />)}
-          <div ref={messagesEndRef} />
-        </ul>
-          <form className='send-container' onSubmit={handleSubmit}>
-                <input className='chat-input search' type="text" maxLength='100' ref={messageInputRef}/>
-                <button type='submit' className='button-mini' style={{color: 'white'}}>&#x27A4;</button>
-          </form>
-    </div>
-=======
   return (
     <div className='chat container h-[100%] overflow-hidden space-y-2
                     border-none'>
@@ -130,7 +70,6 @@ const Chat = ({room, name, playerId}) => {
      
     </div>
     
->>>>>>> master
   )
 }
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,14 +2,10 @@
 
 const Message = ({message, playerId}) => {
   return (
-<<<<<<< HEAD
-      <li className='chat-message' key={message.val().key}><div className={!message.val().author === playerId ? 'chat-player' : 'chat-player mine'}>{message.val().name.charAt(0).toUpperCase() + message.val().name.charAt(1)}</div><p>{message.val().body}</p></li>
-=======
       <li className='flex h-auto space-x-2 items-center w-full '>
         <div className="w-10 h-10 flex items-center justify-center border-4 border-brdr rounded-full">{message.val().name.charAt(0).toUpperCase() + message.val().name.charAt(1)}</div>
         <p className="bg-brdr rounded-lg max-w-[70%]  break-words p-2">{message.val().body}</p>
       </li>
->>>>>>> master
   )
 }
 
